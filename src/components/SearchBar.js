68dcf196fab1100479a3c3fb11df7ef9be78aaff
kgerrard <keegangerrard@gmail.com>
2019-04-26 19:25:59 -0500

    Start implementation of API requests

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const BASE_URL = "http://services.runescape.com/"
 
@@ -6,33 +7,54 @@ class SearchBar extends Component {
     
     constructor(props){
         super(props);
-        this.state = {selected: "itemdb_rs", term: ''};
-
-        this.handleDropDownChange = this.handleDropDownChange.bind(this);
-        this.handleInputChange = this.handleDropDownChange.bind(this);
+        this.state = {selectedVersion: "itemdb_rs", selectedCategory: "", term: ''};
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(){
+    handleSubmit(event){
+        event.preventDefault();
 
-    }
+        let url = BASE_URL + 'm=' + this.state.selectedVersion + '/api/catalogue/items.json?category=' + this.state.selectedCategory;
 
-    handleDropDownChange(event){
-        console.log(this.state.selected);
-        this.setState({selected: event.target.value});
+        axios.get(url ,{
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            },
+            params :{
+                alpha: this.state.term.toLowerCase()[0],
+                page: 1
+            }
+        });
+
+
     }
 
     render() {
         return (
-            <form className="search-bar">
-                <select value={this.state.selected} onChange={this.handleDropDownChange} >
+            <form className="search-bar"  onSubmit={this.handleSubmit}>
+                <select
+                    name={"version-selector"} 
+                    value={this.state.selectedVersion} 
+                    onChange={e => this.setState({selectedVersion: e.target.value})} 
+                >
                     <option value={"itemdb_rs"}>Runescape</option>
                     <option value={"itemdb_oldschool"}>OSRS</option>
                 </select>
-                <input 
+                <select
+                    name={"category-selector"}
+                    value={this.state.selectedCategory}
+                    onChange={e => this.setState({selectedCategory: e.target.value})}
+                >
+                    <option value={"1"}>Miscellaneous</option>
+                    <option value={"2"}>Ammo</option>
+                    <option value={"3"}>Bolts</option>
+                </select>
+                <input
+                    name={"search-bar"} 
                     placeholder="Search the Grand Exchange" 
                     type="text"
                     value={this.state.term}
-                    onChange={e => this.setState({term: event.target.value})}
+                    onChange={e => this.setState({term: e.target.value})}
                 />
                 <button type="submit">Search</button>
             </form> 
