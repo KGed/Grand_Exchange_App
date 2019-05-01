b18789901058206832f65adf92de02f818a0ca7a
kgerrard <keegangerrard@gmail.com>
2019-05-01 17:57:40 -0500

    Implement results table

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,14 +8,15 @@ class SearchBar extends Component {
     
     constructor(props){
         super(props);
-        this.state = {selectedVersion: "itemdb_rs", selectedCategory: "1", term: ''};
+        this.state = {selectedVersion: "itemdb_rs", selectedCategory: "1", term: '', results: []};
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     
     handleSubmit(event){
         event.preventDefault();
 
-        let url = BASE + BASE_URL + 'm=' + this.state.selectedVersion + '/api/catalogue/category.json?category=' + this.state.selectedCategory;
+        let item = this.state.term.toLowerCase();
+        let url = BASE + BASE_URL + 'm=' + this.state.selectedVersion + '/api/catalogue/items.json?category=' + this.state.selectedCategory + '&alpha=' + item[0] + '&page=1';
 
         let data = fetch(url)
         .then(response => {
@@ -25,40 +26,67 @@ class SearchBar extends Component {
         })
 
         data.then(response => {
-            console.log(response);
+            this.setState({results: response.items})
         })
 
     }
 
     render() {
+        // generate table
+        let rows = []
+        for(let item of this.state.results){
+            let itemID = item.id;
+            let cell = []
+            let img = <td key={"img" + itemID}><img src={item.icon_large}></img></td>
+            cell.push(img)
+            cell.push(<td key={"name" + itemID}>{item.name}</td>)
+            cell.push(<td key={"description" + itemID}>{item.description}</td>)
+            rows.push(<tr key={itemID}>{cell}</tr>);
+        }
+
+
+
         return (
-            <form className="search-bar"  onSubmit={this.handleSubmit}>
-                <select
-                    name={"version-selector"} 
-                    value={this.state.selectedVersion} 
-                    onChange={e => this.setState({selectedVersion: e.target.value})} 
-                >
-                    <option value={"itemdb_rs"}>Runescape</option>
-                    <option value={"itemdb_oldschool"}>OSRS</option>
-                </select>
-                <select
-                    name={"category-selector"}
-                    value={this.state.selectedCategory}
-                    onChange={e => this.setState({selectedCategory: e.target.value})}
-                >
-                    <option value={"1"}>Miscellaneous</option>
-                    <option value={"2"}>Ammo</option>
-                    <option value={"3"}>Bolts</option>
-                </select>
-                <input
-                    name={"search-bar"} 
-                    placeholder="Search the Grand Exchange" 
-                    type="text"
-                    value={this.state.term}
-                    onChange={e => this.setState({term: e.target.value})}
-                />
-                <button type="submit">Search</button>
-            </form> 
+            <div>
+                <form className="search-bar"  onSubmit={this.handleSubmit}>
+                    <select
+                        name={"version-selector"} 
+                        value={this.state.selectedVersion} 
+                        onChange={e => this.setState({selectedVersion: e.target.value})} 
+                    >
+                        <option value={"itemdb_rs"}>Runescape</option>
+                        <option value={"itemdb_oldschool"}>OSRS</option>
+                    </select>
+                    <select
+                        name={"category-selector"}
+                        value={this.state.selectedCategory}
+                        onChange={e => this.setState({selectedCategory: e.target.value})}
+                    >
+                        <option value={"1"}>Miscellaneous</option>
+                        <option value={"2"}>Ammo</option>
+                        <option value={"3"}>Bolts</option>
+                        <option value={"9"}>Familiars</option>
+                    </select>
+                    <input
+                        name={"search-bar"} 
+                        placeholder="Search the Grand Exchange" 
+                        type="text"
+                        value={this.state.term}
+                        onChange={e => this.setState({term: e.target.value})}
+                    />
+                    <button type="submit">Search</button>
+                </form>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Icon</th>
+                            <th>Item</th>
+                            <th>Description</th>
+                        </tr>
+                        {rows}
+                    </tbody>
+                </table>
+            </div> 
         );
     }
 
