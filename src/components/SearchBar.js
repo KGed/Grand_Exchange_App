17ffb3e98fd7fbdd73ff739eef63e8b3ba5ffd13
kgerrard <keegangerrard@gmail.com>
2019-05-01 19:38:03 -0500

    Implement dynamic select list for categories

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,32 +3,36 @@ import axios from 'axios';
 
 const BASE = `https://cors-anywhere.herokuapp.com/`
 const BASE_URL = "http://services.runescape.com/"
+const CATEGORIES = ['Miscellaneous', 'Ammo', 'Arrows', 'Bolts', 'Construction Materials', 'Construction Projects', 'Cooking Ingredients', 'Costumes', 'Crafting Materials',
+                    'Familiars', 'Farming Produce', 'Fletching Materials', 'Food and Drink', 'Herblore Materials', 'Hunting Equipment', 'Hunting Produce', 'Jewellery', 'Mage Armour',
+                    'Mage Weapons', 'Melee Armour- Low Level', 'Melee Armour Mid-Level', 'Melee Armour - High Level', 'Melee Weapons - Low Level', 'Melee Weapons - Mid Level',
+                    'Melee Weapons - High Level', 'Mining and Smithing', 'Potions', 'Prayer Armour', 'Prayer Materials', 'Range Armour', 'Range Weapons', 'Runecrafting', 'Runes, Spells, and Teleports',
+                    'Seeds', 'Summoning Scrolls', 'Tools and Containers', 'Woodcutting product', 'Pocket Items']
 
 class SearchBar extends Component {
     
     constructor(props){
         super(props);
-        this.state = {selectedVersion: "itemdb_rs", selectedCategory: "1", term: '', results: []};
+        this.state = {selectedVersion: "itemdb_rs", selectedCategory: "1", term: '', results: [], numOfResults: 0, selectedNumOfResults: 25};
         this.handleSubmit = this.handleSubmit.bind(this);
     }
     
     handleSubmit(event){
         event.preventDefault();
 
+
         let item = this.state.term.toLowerCase();
         let url = BASE + BASE_URL + 'm=' + this.state.selectedVersion + '/api/catalogue/items.json?category=' + this.state.selectedCategory + '&alpha=' + item[0] + '&page=1';
 
-        let data = fetch(url)
+        fetch(url)
         .then(response => {
             if(response.ok){
                 return response.json();
             }
-        })
-
-        data.then(response => {
-            this.setState({results: response.items})
-        })
-
+        }).then(response => {
+            this.setState({results: response.items, numOfResults: response.total})
+            console.log(response)
+        })   
     }
 
     render() {
@@ -40,12 +44,26 @@ class SearchBar extends Component {
             let img = <td key={"img" + itemID}><img src={item.icon_large}></img></td>
             cell.push(img)
             cell.push(<td key={"name" + itemID}>{item.name}</td>)
+
+            if(item.current.price[item.current.price.length-1] == 'k'){
+                cell.push(<td key={"price" + itemID}>{item.current.price}</td>)
+            }else{
+                cell.push(<td key={"price" + itemID}>{ item.current.price + "gp"}</td>)
+            }
+
+            
             cell.push(<td key={"description" + itemID}>{item.description}</td>)
             rows.push(<tr key={itemID}>{cell}</tr>);
         }
 
-
-
+        // generate category list
+        let categoryOptions = []
+        for(let i = 0; i < CATEGORIES.length; i++){
+            let cell = []
+            cell.push(<option key={i} value={i + 1}>{CATEGORIES[i]}</option>)
+            categoryOptions.push(cell)
+        }
+        
         return (
             <div>
                 <form className="search-bar"  onSubmit={this.handleSubmit}>
@@ -62,10 +80,15 @@ class SearchBar extends Component {
                         value={this.state.selectedCategory}
                         onChange={e => this.setState({selectedCategory: e.target.value})}
                     >
-                        <option value={"1"}>Miscellaneous</option>
-                        <option value={"2"}>Ammo</option>
-                        <option value={"3"}>Bolts</option>
-                        <option value={"9"}>Familiars</option>
+                       {categoryOptions}
+                    </select>
+                    <select
+                        name={"resultsAmount-selector"}
+                        value={this.state.selectedNumOfResults}
+                        onChange={e => this.setState({selectedNumOfResults: e.target.value})}
+                    >
+                        <option value={25}>25</option>
+                        <option value={50}>50</option>
                     </select>
                     <input
                         name={"search-bar"} 
@@ -81,6 +104,7 @@ class SearchBar extends Component {
                         <tr>
                             <th>Icon</th>
                             <th>Item</th>
+                            <th>Price</th>
                             <th>Description</th>
                         </tr>
                         {rows}
